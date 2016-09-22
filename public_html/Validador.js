626d4429f3cce1c2d568b844e0188dc06a3d9651
hack <hack@hack>
2016-09-22 10:53:15 -0300

    Atualizado joseffer

---
+++ public_html/Validador.js
@@ -0,0 +1,11 @@
+var Pessoa = function(nome, ra , sexo ,idade, endereco , telefone ,email ) {
+     this.nome =  nome;
+     this.ra=ra;
+     this.sexo=sexo;
+     this.idade=idade;
+     this.endereco= endereco;
+     this.telefone=telefone;
+     this.email=email;
+     
+};
+

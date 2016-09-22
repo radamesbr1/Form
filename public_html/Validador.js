eb39f3c82f3d7ef45e8b74d281bdca07612fc1e3
p157018 <p157018@li005>
2016-09-22 11:24:40 -0300

    Formulario criado

--- public_html/Validador.js
+++ public_html/Validador.js
@@ -1,11 +1,28 @@
 var Pessoa = function(nome, ra , sexo ,idade, endereco , telefone ,email ) {
-     this.nome =  nome;
-     this.ra=ra;
-     this.sexo=sexo;
-     this.idade=idade;
-     this.endereco= endereco;
-     this.telefone=telefone;
-     this.email=email;
+     this.nomejs =  nome;
+     this.rajs=ra;
+     this.sexojs=sexo;
+     this.idadejs=idade;
+     this.enderecojs= endereco;
+     this.telefonejs=telefone;
+     this.emailjs=email;
      
 };
 
+var teste = new Pessoa(nome, ra , sexo ,idade, endereco , telefone ,email)
+
+function PegarDados(){
+    
+  
+  nome= document.getElementsByName(nome);
+  ra= document.getElementsByName(ra);
+  sexo= document.getElementsByName(sexo);
+  idade= document.getElementsByName(idade);
+  endereco= document.getElementsByName(endereco);
+  telefone= document.getElementsByName(telefone);
+  email=document.getElementsByName(email);
+};
+
+function Alerta(){
+    
+};
